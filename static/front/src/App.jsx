8d8de7dc644ae13d9e693441ac6499073ff940ef
Ved-Dixit <veddixit195@gmail.com>
2025-03-17 03:06:38 +0530

    front

--- static/front/src/App.jsx
+++ static/front/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
   };
 
   const handleSubmit = async () => {
-    const response = await fetch("http://127.0.0.1:5000/run_pil_generator", {
+    const response = await fetch("https://gctx3787.up.railway.app/run_pil_generator", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
